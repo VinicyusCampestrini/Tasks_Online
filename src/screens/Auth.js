19d85f25218500efa165399f0576293350b8213a
Vinicyus Camopestrini <vinicyus.campes@gmail.com>
2020-02-13 15:21:07 -0300

    aula 114

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -6,8 +6,12 @@ import backgroundImage from '../../assets/imgs/login.jpg'
 export default class Auth extends Component {
 
     state = {
+        name: '',
+        sobrenome: '',
         email: '',
         password: '',
+        confirmPassword: '',
+        stageNew: true,
     }
 
     render() {
@@ -16,13 +20,27 @@ export default class Auth extends Component {
             style={styles.backgound}>
                 <Text style={styles.title}>Tasks</Text>
                 <View style={styles.formConteiner}>
+                    <Text style={styles.subtitle}>
+                        {this.state.stageNew ? 'Crie sua conta agora mesmo' : 'Digite seu login e senha'}
+                    </Text>
+                    {this.state.stageNew && 
+                        <TextInput placeholder = 'Nome' value={this.state.name}
+                            style={styles.input} onChangeText={ name => this.setState({ name })}/>}
+                    {this.state.stageNew && 
+                        <TextInput placeholder = 'Sobrenome' value={this.state.sobrenome}
+                            style={styles.input} onChangeText={ sobrenome => this.setState({ sobrenome })}/>}
                     <TextInput placeholder = 'E-mail' value={this.state.email}
                         style={styles.input} onChangeText={ email => this.setState({ email })}/>
                     <TextInput placeholder = 'Senha' value={this.state.password}
-                        style={styles.input} onChangeText={ password => this.setState({ password })}/>
+                        style={styles.input} secureTextEntry={true}  onChangeText={ password => this.setState({ password })}/>
+                        {this.state.stageNew && 
+                        <TextInput placeholder = 'Confime sua senha' value={this.state.confirmPassword}
+                            style={styles.input} onChangeText={ confirmPassword => this.setState({ confirmPassword })}/>}
                     <TouchableOpacity>
                         <View style={styles.button}>
-                            <Text style={styles.buttonText}>Entrar</Text>
+                        <Text style={styles.buttonText}>{
+                            this.state.stageNew ? 'Cadastre-se' : 'Entre'
+                        }</Text>
                         </View>
                     </TouchableOpacity>
                 </View>
@@ -51,6 +69,7 @@ const styles = StyleSheet.create({
         marginTop: 10,
         backgroundColor: '#FFF',
         padding: Platform.OS == 'ios' ? 15 : 10,
+        textAlign: 'center'
     },
     formConteiner: {
         backgroundColor: 'rgba(0, 0, 0, 0.6)',
@@ -62,12 +81,18 @@ const styles = StyleSheet.create({
         marginTop: 10,
         padding: 10,
         alignItems: 'center',
-
     },
     buttonText: {
         fontFamily: commonStyles.fontFamily,
         color: '#fff',
         fontSize: 20,
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 20,
+        margin: 10,
 
-    }
+    },
 })
